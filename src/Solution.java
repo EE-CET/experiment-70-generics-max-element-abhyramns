f05c3ecacc99845fb08bc829807674e74b22ffbb
abhyramns <abhyramns@gmail.com>
2026-03-06 11:15:33 +0530

    code updated

--- src/Solution.java
+++ src/Solution.java
@@ -1,39 +1,41 @@
 import java.util.Scanner;
 
 public class Solution {
+    
+    // TODO: Write the generic method findMax(T[] array) here
+    // Hint: The type parameter T must extend Comparable<T>
+    
     public static <T extends Comparable<T>> T findMax(T[] array) {
-        if (array == null || array.length == 0) {
-            return null;
-        }
-        
         T max = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i].compareTo(max) > 0) {
-                max = array[i];
+        for (T element : array) {
+            if (element.compareTo(max) > 0) {
+                max = element;
             }
         }
         return max;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        int n = scanner.nextInt();
         Integer[] intArray = new Integer[n];
         for (int i = 0; i < n; i++) {
-            intArray[i] = sc.nextInt();
+            intArray[i] = scanner.nextInt();
         }
 
-        int m = sc.nextInt();
-        String[] stringArray = new String[m];
+        // TODO: Call findMax on intArray and print the result
+        // Format: "Max Integer: [val]"
+        int m = scanner.nextInt();
+        String[] strArray = new String[m];
         for (int i = 0; i < m; i++) {
-            stringArray[i] = sc.next();
+            strArray[i] = scanner.next();
         }
 
+        // TODO: Call findMax on strArray and print the result
+        // Format: "Max String: [val]"
         System.out.println("Max Integer: " + findMax(intArray));
-        System.out.println("Max String: " + findMax(stringArray));
-        
-        sc.close();
-    }
-}
+        System.out.println("Max String: " + findMax(strArray));
 
+        scanner.close();
+    }
